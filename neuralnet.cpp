2c00b18768c6cd7a75cf640ea115b8a485dee215
Neeha  Sajja <neeha9chowdary@gmail.com>
2023-01-25 19:19:40 +0100

    Add files via upload

--- neuralnet.cpp
+++ neuralnet.cpp
@@ -1,48 +1,9 @@
-#include <vector>
-#include <iostream>
-#include <cstdlib>
-#include <cmath>
-#include <fstream>
+//here we implement the methods  themselves
+#include "neuralnet.hpp"
 
-#include <sstream>
+void data::set_feature_vector(std::vector<int8_t> *);
+void data::append_to_feature_vector(unit8_t);
+void data::set_label();
+void data::set_enumerated_label();
 
-using namespace std;
-
-class trajectoryprediction
-{
-public:
-  (const vector<unsigned> &topology);
-  void feedForward(const vector<double> &inputVals);
-  void backProp(const vector<double> &targetVals);
-  void getResults(vector<double> &resultVals) const;
-
-private:
-};
-
-// set environment variable for data
-     const char *path="/home/neehasajja/file.txt";
-     std::ofstream file(path); //open in constructor
-     std::string data("data to wrtite to file");
-     file << data;
-     std::filesystem::path -
-
-
-// get config
-const char config[] = "url=http://example.com\n"
-                  "file=main.exe\n"
-                  "true=0";
-
-std::istringstream is_file(config);
-
-std::string line;
-while( std::getline(is_file, line) )
-{
-std::istringstream is_line(line);
-std::string key;
-if( std::getline(is_line, key, '=') )
-{
-std::string value;
-if( std::getline(is_line, value) )
-  store_line(key, value);
-}
-}
+std::vector<int8_t> * data::get_featre_vector();
