6066672b7b1251c60a147ef9e13d763514a0e1fd
Neeha  Sajja <neeha9chowdary@gmail.com>
2023-01-27 16:11:08 +0100

    Add files via upload

---
+++ input_layer.hpp
@@ -0,0 +1,15 @@
+#ifndef  __INPUT_LAYER_HPP
+#define  __INPUT_LAYER_HPP
+
+#include "layer.hpp"
+#include "data.hpp"
+
+class InputLayer : public Layer
+{
+
+  public:
+  InputLayer(int prev, int current) : Layer(prev, current){}
+  void setLayerOutputs(data *d);
+}
+
+#endif
