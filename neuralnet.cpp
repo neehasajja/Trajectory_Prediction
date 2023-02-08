8d131c6bcf079b5379bc08dc0c3f4987477face7
Neeha  Sajja <neeha9chowdary@gmail.com>
2023-02-08 21:29:26 +0100

    Delete neuralnet.cpp

--- neuralnet.cpp
+++
@@ -1,9 +0,0 @@
-//here we implement the methods  themselves
-#include "neuralnet.hpp"
-
-void data::set_feature_vector(std::vector<int8_t> *);
-void data::append_to_feature_vector(unit8_t);
-void data::set_label();
-void data::set_enumerated_label();
-
-std::vector<int8_t> * data::get_featre_vector();
